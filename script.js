b53532c81ac23922b1467d1a753375d702e31321
Lorna Munanie <lornamunanie32@gmail.com>
2025-08-26 14:05:11 +0300

    updated endpoints

--- script.js
+++ script.js
@@ -72,9 +72,9 @@ document.addEventListener('DOMContentLoaded', () => {
         try {
             // API_BASE_URL: Change between local testing and live deployment
             // For local testing:
-            const API_BASE_URL = "http://127.0.0.1:8000";
+            //const API_BASE_URL = "http://127.0.0.1:8000";
             // For live deployment:
-            //const API_BASE_URL = "https://nutriapp-backend-mnnq.onrender.com";
+            const API_BASE_URL = "https://nutriapp-backend-mnnq.onrender.com";
 
             const apiUrl = `${API_BASE_URL}/recommend_meal?${queryParams.join('&')}`;
             console.log('Fetching recommendations from:', apiUrl);
